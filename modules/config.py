09307b2d6bb4b1eaed1e0edcdf64dcf8fa3ffd78
alexgurliya <alexgurliya@gmail.com>
2025-01-05 23:05:42 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,7 @@
-BOT_NAME = "Moni Uploader 2"
-API_ID    = 24495656
-API_HASH  = "61afcf68c6429714dd18acd07f246571"
-BOT_TOKEN = "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+# config.py
+import os
+BOT_NAME = os.environ.get('BOT_NAME')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+API_ID = int(os.environ.get('API_ID'))
+API_HASH = os.environ.get('API_HASH')
+MONGO_URI = os.environ.get('MONGO_URI')
