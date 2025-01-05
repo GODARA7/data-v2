4da9a3661b56d564d8dfb0744d6916f891abc92e
alexgurliya <alexgurliya@gmail.com>
2025-01-05 23:59:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-# if [ -z $UPSTREAM_REPO ]
-# then
-  echo "Cloning main Repository"
-  # git clone https://github.com/alexgurliya/Black /data-v2
-# else
-#   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-#   git clone $UPSTREAM_REPO /data-v2
-# fi
-# cd /data-v2
-# pip3 install -U -r requirements.txt
+echo "Cloning Repo...."
+git clone https://github.com/alexgurliya/data-v2 /data-v2
+cd /data-v2
+pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 modules/main.py
